e04bb831356dcca82045915c049be6eb2474ef96
Bavizhev <ilyas4bavizhev@gmail.com>
2023-11-05 07:05:08 +0300

    Stub and tests for add

--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,5 +1,24 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class PhoneBookTest {
 
+    private PhoneBook phoneBook;
+    @BeforeEach
+    public void setup() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void testAddContact() {
+        int initialCount = phoneBook.add("John", "1234567890");
+        assertEquals(1, initialCount);
+
+        int updatedCount = phoneBook.add("Alice", "9876543210");
+        assertEquals(2, updatedCount);
+    }
 }
