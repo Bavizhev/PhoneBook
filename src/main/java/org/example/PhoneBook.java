b7fc2976b152e63c1caa8407d10e96b918b3d51d
Bavizhev <ilyas4bavizhev@gmail.com>
2023-11-05 07:06:23 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,8 +1,17 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> contacts;
+
+    public PhoneBook() {
+        contacts = new HashMap<>();
+    }
 
     public int add(String name, String number) {
-        return 0;
+        contacts.put(name, number);
+        return contacts.size();
     }
 }
